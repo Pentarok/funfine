08803c20292ca77c3e24c471a7ba071d1387e173
Pentarok <makpentarok@gmail.com>
2024-11-01 23:25:53 +0300

    ErrorBoundary update

--- ErrorBoundary.jsx
+++ ErrorBoundary.jsx
@@ -19,7 +19,9 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <div style={{minHeight:'100vh'}}>
+        <h1 style={{color:'white'}}>Something went wrong.</h1>;
+        </div>
     }
 
     return this.props.children; 
