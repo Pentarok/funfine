c53ad913532d57c98c7a8967682118b8efaf20f3
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-12-12 13:04:52 +0300

    Update DatabaseCheck.js

--- src/DatabaseCheck.js
+++ src/DatabaseCheck.js
@@ -4,14 +4,14 @@ export function useDatabaseCheck() {
   const [isConnected, setIsConnected] = useState(null);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(true); // Add loading state
-  const serverUri = import.meta.env.VITE_BACKEND_URL;
+  const serverUri = "https://funfine-api-git-main-mak-pentaroks-projects.vercel.app/api/check-db-connection";
 
   useEffect(() => {
     async function checkConnection() {
       setLoading(true); // Set loading to true when starting the check
       try {
         
-        const response = await fetch(`${serverUri}/api/check-db-connection`);
+        const response = await fetch(`${serverUri}`);
         console.log(response);
         const data = await response.json();
 
